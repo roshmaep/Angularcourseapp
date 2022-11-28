b5ba078bc9e88212aa18dedeea044e72fd7234fc
roshmaep <roshmaep1999@gmail.com>
2022-11-28 12:10:27 +0530

    add read value

--- src/app/addcourse/addcourse.component.ts
+++ src/app/addcourse/addcourse.component.ts
@@ -1,3 +1,4 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./addcourse.component.css']
 })
 export class AddcourseComponent {
+
+  courseTitle=""
+  courseDescription=""
+  courseDuration=""
+  courseDate=""
+  courseVenue=""
+
+  readValues=()=>
+  {
+  let data:any={"courseTitle":this.courseTitle,"courseDescription":this.courseDescription,"courseDuration":this.courseDuration,"courseDate":this.courseDate,"courseVenue":this.courseVenue}
+  console.log(data)
+  }
 
 }
