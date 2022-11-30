970a849935db8100eb23f3ab0048262f86719592
roshmaep <roshmaep1999@gmail.com>
2022-11-30 10:06:28 +0530

    pass data

--- src/app/addcourse/addcourse.component.ts
+++ src/app/addcourse/addcourse.component.ts
@@ -1,5 +1,6 @@
 import { ThisReceiver } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-addcourse',
@@ -15,10 +16,33 @@ export class AddcourseComponent {
   courseDate=""
   courseVenue=""
 
+  constructor(private api:ApiService){}
+
   readValues=()=>
   {
-  let data:any={"courseTitle":this.courseTitle,"courseDescription":this.courseDescription,"distributor":this.distributor,"courseDuration":this.courseDuration,"courseDate":this.courseDate,"courseVenue":this.courseVenue}
+  let data:any={"courseTitle":this.courseTitle,"courseDescription":this.courseDescription,"courseDuration":this.courseDuration,"distributor":this.distributor,"courseDate":this.courseDate,"courseVenue":this.courseVenue}
   console.log(data)
+  this.api.addCourse(data).subscribe(
+
+    (response:any)=>{
+
+      console.log(response)
+      if(response.status =="success"){
+        alert("course added successfully")
+        this.courseTitle=""
+        this.courseDate=""
+        this.courseDescription=""
+        this.courseDuration=""
+        this.courseVenue=""
+        this.distributor=""
+      }
+      else{
+        alert("something wrong")
+      }
+
+    }
+
+  )
   }
 
 }
